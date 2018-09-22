9aad74a2819d1edacac3ccc93cbd19b1ac331337
jlraes <jlraes@gmail.com>
2018-09-22 16:28:47 +0200

    final check ok

--- .sfdx/typings/lwc/apex/INDICATOR_Controller.d.ts
+++ .sfdx/typings/lwc/apex/INDICATOR_Controller.d.ts
@@ -1,3 +1,6 @@
 declare module "@salesforce/apex/INDICATOR_Controller.getSObjectLabel" {
   export default function getSObjectLabel(param: {sObjName: any}): Promise<any>;
 }
+declare module "@salesforce/apex/INDICATOR_Controller.getIndicators" {
+  export default function getIndicators(param: {recId: any, objectName: any}): Promise<any>;
+}
